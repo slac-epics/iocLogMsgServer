f3ef002dea52fee8709b27e4597f9f2a0db173ed
niafong <>
2012-08-21 22:26:43 +0000

    Use env variables /usr/local/lcls/tools and /usr/local/lcls/package/oracle/product/11.1.0.6/client instead of hardcoded paths.

--- envForBuild.bash
+++ envForBuild.bash
@@ -1,9 +1,9 @@
 # current oracle env seems to have what it takes!
-source /afs/slac/g/lcls/tools/oracle/oracleSetup.bash
+#source /afs/slac/g/lcls/tools/oracle/oracleSetup.bash
+source $TOOLS/oracle/oracleSetup.bash
 
 # now add on stuff for ProC
-export ORACLE_HOME=/afs/slac/package/oracle/d/linux/11.1.0
+#export ORACLE_HOME=/afs/slac/package/oracle/d/linux/11.1.0
 export PATH=$ORACLE_HOME/bin:${PATH}
 export LD_LIBRARY_PATH=$ORACLE_HOME/precomp/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$ORACLE_HOME/lib:$LD_LIBRARY_PATH
-
